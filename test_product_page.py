e45caebd7f00f1cc8a42e199b97e12181ab9258d
Alyona Natsyuk <alyona.natsyuk@requestum.com>
2020-09-02 13:52:06 +0300

    added pytest.ini and check links

--- test_product_page.py
+++ test_product_page.py
@@ -1,3 +1,4 @@
+import pytest
 from .pages.product_page import ProductPage
 
 def test_guest_can_go_to_first_product_page(browser):
@@ -19,3 +20,25 @@ def test_guest_can_go_to_second_product_page(browser):
     page.should_be_success_message()
     page.should_be_message_product_added()
     page.should_be_message_price_added()
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+                                               "/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    link = f"{link}"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_the_basket()
+    page.solve_quiz_and_get_code()
+    page.should_be_success_message()
+    page.should_be_message_product_added()
+    page.should_be_message_price_added()
+
