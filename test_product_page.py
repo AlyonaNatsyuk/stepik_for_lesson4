51bc490b2f997b4fb2ebfdd8ec1888eca0f6869a
Alyona Natsyuk <alyona.natsyuk@requestum.com>
2020-09-02 13:18:02 +0300

    fixed my errors in tests

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,4 @@
 from .pages.product_page import ProductPage
-import time
 
 def test_guest_can_go_to_first_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
@@ -7,7 +6,6 @@ def test_guest_can_go_to_first_product_page(browser):
     page.open()
     page.add_product_to_the_basket()
     page.solve_quiz_and_get_code()
-
     page.should_be_success_message()
     page.should_be_message_product_added()
     page.should_be_message_price_added()
@@ -18,7 +16,6 @@ def test_guest_can_go_to_second_product_page(browser):
     page.open()
     page.add_product_to_the_basket()
     page.solve_quiz_and_get_code()
-
     page.should_be_success_message()
     page.should_be_message_product_added()
     page.should_be_message_price_added()
